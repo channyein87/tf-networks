98ee41f33e4a3fc0d7bc21ed47eb0639c0c70482
Chan Nyein Ko Ko <channyein87@hotmail.com>
2020-12-13 11:42:17 +1100

    add prod vpc

--- aws/vpc/prod/variables.tf
+++ aws/vpc/prod/variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 
 variable "profile" {
     type = string
-    default = "kb-shared"
+    default = "kb-prod"
 }
 
 variable "prefix" {
@@ -20,26 +20,26 @@ variable "environment" {
 
 variable "cidr" {
     type = string
-    default = "10.0.0.0/16"
+    default = "10.8.0.0/16"
 }
 
 variable "public-subnets" {
     default = {
-        ap-southeast-2a = "10.0.101.0/24"
-        ap-southeast-2b = "10.0.102.0/24"
+        ap-southeast-2a = "10.8.101.0/24"
+        ap-southeast-2b = "10.8.102.0/24"
     }
 }
 
 variable "private-subnets" {
     default = {
-        ap-southeast-2a = "10.0.1.0/24"
-        ap-southeast-2b = "10.0.2.0/24"
+        ap-southeast-2a = "10.8.1.0/24"
+        ap-southeast-2b = "10.8.2.0/24"
     }
 }
 
 variable "attach-subnets" {
     default = {
-        ap-southeast-2a = "10.0.201.0/24"
-        ap-southeast-2b = "10.0.202.0/24"
+        ap-southeast-2a = "10.8.201.0/24"
+        ap-southeast-2b = "10.8.202.0/24"
     }
 }
